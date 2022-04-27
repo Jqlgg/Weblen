e32f946251dcc4d075e025d46f2fcdf244c0fb1a
Jqlgg <1224686994@qq.com>
2022-04-27 20:51:35 +0800

    weblen1

---
+++ ServletHttp/src/main/java/servlet/ServletHttp.java
@@ -0,0 +1,15 @@
+package servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+@WebServlet("/demo2")
+public class ServletHttp extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("tsts");
+    }
+}
